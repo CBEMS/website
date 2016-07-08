2d0b6c9c9486f2e8351e9454df2afc1d1988712e
Ahmed H. Moneeb <ahmedhamdy395@gmail.com>
2016-07-08 17:27:09 +0200

    added abrar work

--- javascript/allPages.js
+++ javascript/allPages.js
@@ -1,32 +1,85 @@
-document.getElementById("home").onclick= function() {
-   if (document.getElementById("homepage").style.display=="none"){
-       document.getElementById("homepage").style.display=="inline-block";
-       document.getElementById("billing").style.display=="none";       
-   }
-}
-document.getElementById("bill").onclick= function() {
-   if (document.getElementById("billing").style.display=="none"){
-       document.getElementById("homepage").style.display=="inline-block";
-       document.getElementById("billing").style.display=="none";       
-   }
- 
-}
+
 
 // JQUERY CODE
 
 $(document).ready(function(){
     
-    
-    // height of the content
-    $("#content").height( $( window ).height() );
-    
-    // position and set the size of the header 
-    $("#header").width( $( window ).width()-80 ); // set the size at starting (80 is the menu width)
+    // position the header 
+    $("#header").width( $( window ).width()-80 ); // position at starting (80 is the menu width)
     $(window).on('resize', function(){  // position on resizing the window
         var windowWidth = $( window ).width();
         $("#header").width( windowWidth-80 );
     });
+
     
-    
-    
+
+    //power consumption chart:
+        window.onload = function () {
+                var chart = new CanvasJS.Chart("chartContainer", x);
+                chart.render();
+        }
+        var x= {
+				
+				animationEnabled: true,
+				theme: "theme3",
+				backgroundColor:"rgb(225, 225, 225)",
+				
+				data: [
+				{
+					type: "doughnut",
+					indexLabelFontFamily: "Garamond",
+					indexLabelFontSize: 20,
+					startAngle: 0,
+					indexLabelFontColor: "dimgrey",
+					indexLabelLineColor: "darkgrey",
+					toolTipContent: "{y} %",
+                    /*dataPOINTS:
+                    {y: total power consumtion of a blook , indexLabel:"blook name: y%"}*/
+                    //example
+					dataPoints: [
+					{ y: 51.04, indexLabel:"Room (1): {y}%" },
+					{ y: 10.83, indexLabel:"Room (2): {y}%" },
+					{ y: 30.20, indexLabel: "Room (3): {y}%" },
+					{ y: 10.11, indexLabel: "Room (4): {y}%" },
+					{ y: 20.29, indexLabel: "Room (5): {y}%" },
+					{ y: 10.53, indexLabel: "Room (6): {y}%" }
+                    
+					]
+				
+            }
+				]
+			}
+            
+            //var i,sum=0;
+            //for(i=0;i<x.data[0].dataPoints.length;i++){
+            //    var z=x.data[0].dataPoints[i].y;                               
+            //    sum+= z;
+            //}            
+            //power consumption total power : 
+            /*totalPower is the sum of the power consumption of whole rooms */
+            //var totalPower= parseInt(sum,10);     
+            //$("#cons").before(totalPower);
+            
+        
+        /**********************************/
+        //Billing page
+            /*billReach*/ 
+            var BillReach=function(totalPowerConsumption,time){
+                var billCost=5; //the bill cost as Kw/h        
+                var energy = totalPowerConsumption/time;    
+                var billReach=parseInt(energy/billCost,10);          
+                $("#billCost").prepend(billReach);
+                       
+               var consPrecentage=parseInt(100-(((50-billReach)/50)*100),10);
+                $("#cons").prepend(consPrecentage);    
+                }
+            BillReach(2000,12);
+         
+           $( "#fee" )
+              .keyup(function() {
+                var value = $( this ).val();
+               
+              })
+              .keyup();
+          
 });
