a1d025c6d363f0a629085a026372d26ce82a593b
Ahmed H. Moneeb <ahmedhamdy395@gmail.com>
2016-07-09 14:25:34 +0200

    added test json

---
+++ test/testJson.php
@@ -0,0 +1,13 @@
+<?PHP
+
+$data = array( "device_id" => "18", "day" => "Sun", "start_time" => date('2016-03-01 14:30:00'), "end_time" => date('2016-03-01 14:00:00'), "repetition" => "once" );
+$jsonData = urlencode(json_encode($data));
+$url = "http://lindabot.net23.net/AlaaIOT/api/schedule/set_schedule.php";
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, array("data"=>$jsonData) );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($ch);
+
+echo $result;
+?>
